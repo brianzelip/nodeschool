5de0ce4acb1c46f410720fcd51e8754e7612669f
Brian Zelip <bzelip@gmail.com>
2017-05-13 16:54:20 -0400

    still working - getting unexpected results

--- 2017-05-13/learnyounode/03-my-first-io.js
+++ 2017-05-13/learnyounode/03-my-first-io.js
@@ -12,15 +12,26 @@ Write a program that uses a single synchronous filesystem operation to
 const fs = require('fs');
 
 function returnFileNewlines(file) {
+  // console.log('file', file);
   let fileContents = fs.readFileSync(file, {'encoding': 'utf8'});
-  let fileContentsArr = fileContents.split('\n');
-  console.log('fileContentsArr.length', fileContentsArr.length);
-  // for (i = 0; i<fileContentsArr.length; i++) {
-  //   console.log(fileContentsArr[i]);
-  // }
-  return;
+  // console.log('fileContents', fileContents);
+  let fileContentsArr = fileContents.split('');
+  console.log('fileContentsArr', fileContentsArr);
+
+  let count = 0;
+  for (let i = 0; i<fileContentsArr; i++) {
+    console.log('fileContentsArr[i]', fileContentsArr[i]);
+    if (fileContentsArr[i] == 'h') {
+      count = count + 1;
+    }
+  }
+  // console.log('fileContentsArr.length', fileContentsArr.length);
+  console.log('count', count);
+  return count;
 };
 
 // console.log(returnFileNewlines('/Users/brianzelip/NodeSchool/2017-05-13/learnyounode/test.md'));
 
-returnFileNewlines('/Users/brianzelip/NodeSchool/2017-05-13/learnyounode/test.md');
+// returnFileNewlines('/Users/brianzelip/NodeSchool/2017-05-13/learnyounode/test.md');
+
+returnFileNewlines('./test.md');
